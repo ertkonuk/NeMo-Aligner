53105fd1297e1909e8bdd355eb5eff6de72796fb
abukharin <abukharin@nvidia.com>
2024-07-21 15:16:43 +0000

    Update data format

--- convert.py
+++ convert.py
@@ -5,8 +5,8 @@ with open("data/ifeval_preferences.jsonl", "r") as f:
     data = [json.loads(line) for line in f]
 
 for obj in data:
-    obj["text"] = obj.pop("prompt")
-    print(obj)
+    text = obj["prompt"]
+    obj["text"] = f'<extra_id_0>System\n\n<extra_id_1>User\n{text}\n<extra_id_1>Assistant\n'
     if obj["args"]["instruction_kwargs"][0] is None:
         obj["args"]["instruction_kwargs"] = [{}]
 
@@ -14,10 +14,10 @@ random.seed(0)
 random.shuffle(data)
 
 with open("data/ifeval_train_prompts.jsonl", "w") as f:
-    for entry in data[512:]:
+    for entry in data[128:]:
         f.write(json.dumps(entry) + "\n")
 
 
 with open("data/ifeval_val_prompts.jsonl", "w") as f:
-    for entry in data[:512]:
+    for entry in data[:128]:
         f.write(json.dumps(entry) + "\n")
--- view_data.py
+++ view_data.py
@@ -1,12 +1,17 @@
 import json
 from instruction_following_eval.evaluation_main import InputExample, test_instruction_following_strict, test_instruction_following_loose
 
-# with open("data/extra_id_prefix_end_with_backslash_n_extra_id_1_jsonl/scale_2_filtered_v2_val_prompts.jsonl", "r") as f:
-#     data = [json.loads(line) for line in f]
+with open("data/extra_id_prefix_end_with_backslash_n_extra_id_1_jsonl/scale_2_filtered_v2_val_prompts.jsonl", "r") as f:
+    data = [json.loads(line) for line in f]
+
+print(data[0])
+# exit()
 
 
 with open("data/ifeval_preferences.jsonl", "r") as f:
     data = [json.loads(line) for line in f]
+print(data[0])
+exit()
 
 correct = 0
 for datapoint in data:
