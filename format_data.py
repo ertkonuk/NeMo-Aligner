0aae95f70a6615eceaa3b41f4f0fc1ee41d9faaf
abukharin <abukharin@nvidia.com>
2024-07-23 16:16:09 +0000

    Combine datasets

---
+++ format_data.py
@@ -0,0 +1,30 @@
+import json
+import random
+
+datalist = []
+with open("data/preference.rm9200.reject-rand-chosen-min-0.0.jsonl", "r") as f:
+    for line in f:
+        data = json.loads(line)
+        text = data["prompt"]
+        data["text"] = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+{text}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+""".format(text=text)
+        datalist.append(data)
+
+
+random.seed(0)
+random.shuffle(datalist)
+
+with open("data/llama3_train_data.jsonl", "w") as f:
+    for data in datalist[128:]:
+        jsonline = json.dumps(data)
+        f.write(jsonline + "\n")
+
+with open("data/llama3_val_data.jsonl", "w") as f:
+    for data in datalist[:128]:
+        jsonline = json.dumps(data)
+        f.write(jsonline + "\n")
--- fromat_data.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("data/ifeval_train_prompts.jsonl", "r") as f:
-    for line in f:
-        data = json.loads(line)
-        data["text"] = None
-        exit()
